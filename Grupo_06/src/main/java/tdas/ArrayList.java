7151d4a4c0a44080f951e57e41cc548ac9081ef0
ZarateSofia <szarate@espol.edu.ec>
2023-07-12 16:19:08 -0500

    tda arraylist

--- Grupo_06/src/main/java/tdas/ArrayList.java
+++ Grupo_06/src/main/java/tdas/ArrayList.java
@@ -6,11 +6,11 @@ package tdas;
 
 import java.io.Serializable;
 import java.util.Iterator;
-import java.util.Random;
 
 /**
  *
  * @author DELL
+ * @param <E>
  */
 public class ArrayList<E> implements List<E>, Serializable {
     private E[] elements;
@@ -18,7 +18,6 @@ public class ArrayList<E> implements List<E>, Serializable {
     private int effectiveSize;
 
     public ArrayList() {
-        // elements = new E[100]; // NO vale
         elements = (E[]) new Object[MAX_SIZE];
         effectiveSize = 0;
     }
@@ -42,7 +41,6 @@ public class ArrayList<E> implements List<E>, Serializable {
         return effectiveSize == 0;
     }
 
-//    @Override
     public void clear() {
         if(effectiveSize>0){
             E[] l2=(E[]) new Object[MAX_SIZE];
@@ -53,33 +51,6 @@ public class ArrayList<E> implements List<E>, Serializable {
         }    
     }
 
-//    @Override
-    public boolean addFirst(E element) {
-        if (element == null) {
-            //throw new NullPointerException("Null elements are not accepted");
-            return false;
-        } else if (isEmpty()) {
-
-            // si se quiere hacer el bacán
-            // elements[effectiveSize++] = element;
-            // con humildad
-            elements[effectiveSize] = element;
-            effectiveSize++;
-
-            return true;
-        }
-        if (isFull()) {
-            addCapacity();
-        }
-        // empujar para hacer espacio. Bit shifting
-        for (int i = effectiveSize-1; i >= 0; i--) {
-            elements[i + 1] = elements[i];
-        }
-        elements[0] = element;
-        effectiveSize++;
-        return true;
-    }
-
     @Override
     public boolean addLast(E element) {
         if (element == null) {
@@ -88,40 +59,10 @@ public class ArrayList<E> implements List<E>, Serializable {
         if (isFull()) {
             addCapacity();
         }
-        //con humildad
         elements[effectiveSize] = element;
         effectiveSize++;
-        // con orgullo
-//        elements[effectiveSize++] = element;
         return true;
     }
-
-//    @Override
-    public E removeFirst() {
-        if(effectiveSize==0){
-            return null;
-        }
-        E element=elements[0];
-        elements[0]=null;
-        for(int i=0;i<effectiveSize;i++){
-            elements[i]=elements[i+1];
-        }
-        effectiveSize--;
-        return element;
-    }
-
-//    @Override
-    public E removeLast() {
-        if(effectiveSize==0){
-            return null;
-        }
-        E element=elements[effectiveSize-1];
-        elements[effectiveSize]=null;        
-        effectiveSize--;
-        return element;
-    }
-
-
 
     @Override
     public void add(int index, E element) {
@@ -134,14 +75,13 @@ public class ArrayList<E> implements List<E>, Serializable {
 //        }
 //        for (int i = effectiveSize; i > index; i--) {
 //            elements[i] = elements[i - 1];
-//            // elements[i+1] = elements[i]; MAL
 //        }
 //        elements[index] = element;
 //        effectiveSize++;
 //        return true;
     }
 
-//    @Override
+    @Override
     public E remove(int index) {
         if(index>=effectiveSize || index<0){
             return null;
@@ -163,21 +103,9 @@ public class ArrayList<E> implements List<E>, Serializable {
         return elements[index];
     }
 
-//    @Override
-    public E set(int index, E element) {
-        if(index<0 || element==null|| index>=effectiveSize){
-            return null;
-        }else{
-            elements[index]=element;
-            return element;
-        }    
-    }
-
     private void addCapacity() {
         MAX_SIZE = MAX_SIZE * 2;
         E[] newElements = (E[]) new Object[MAX_SIZE];
-        // copiando los elementos del arreglo viejo al nuevo
-//        for (int i= 0; i<effectiveSize; i++) {
         for (int i = 0; i < elements.length; i++) {
             newElements[i] = elements[i];
         }
@@ -186,76 +114,6 @@ public class ArrayList<E> implements List<E>, Serializable {
 
     private boolean isFull() {
         return effectiveSize == MAX_SIZE;
-    }
-
-//    @Override
-    public void reverse() {
-        int start = 0;
-        int end = effectiveSize - 1;
-        while (start < end) {
-            E temp = elements[start];
-            elements[start] = elements[end];
-            elements[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
-//    @Override
-    public List<E> subList(int from, int to) {
-        if (from < 0 || from >= effectiveSize || to < 0 || to > effectiveSize || from > to) {
-            throw new IndexOutOfBoundsException("Invalid index range");
-        }
-
-        ArrayList<E> sublist = new ArrayList<>();
-        for (int i = from; i < to; i++) {
-            sublist.addLast(elements[i]);
-        }
-        return sublist;
-    }
-
-//    @Override
-    public boolean removeFirstNElements(int n) {
-        if (n <= 0 || n > effectiveSize) {
-            return false;
-        }
-        for (int i = n; i < effectiveSize; i++) {
-            elements[i - n] = elements[i];
-        }
-        effectiveSize -= n;
-        return true;
-    }
-
-//    @Override
-    public void shuffle() {
-        Random random = new Random();
-        for (int i = effectiveSize - 1; i > 0; i--) {
-            int j = random.nextInt(i + 1);
-            E temp = elements[i];
-            elements[i] = elements[j];
-            elements[j] = temp;
-        }
-    }
-
-//    @Override
-    public void rotate(int k) {
-        if (k < 0) {
-            throw new IllegalArgumentException("Invalid rotation amount");
-        }
-
-        int rotation = k % effectiveSize;
-        if (rotation == 0) {
-            return;  // No rotation needed
-        }
-
-        E[] temp = (E[]) new Object[effectiveSize];
-        for (int i = 0; i < effectiveSize; i++) {
-            temp[i] = elements[i];
-        }
-
-        for (int i = 0; i < effectiveSize; i++) {
-            elements[(i + rotation) % effectiveSize] = temp[i];
-        }
     }
 
     @Override
